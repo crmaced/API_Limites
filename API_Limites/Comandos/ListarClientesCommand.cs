f5ba0ecb8554b5e76e7eb11429a6f7890d02ca53
Crislaine Montanha Macedo <crislaine.macedo@voiter.com>
2022-10-20 11:26:44 -0300

    First commit

---
+++ API_Limites/Comandos/ListarClientesCommand.cs
@@ -0,0 +1,23 @@
+﻿namespace API_Limites.Comandos
+{
+    public class ListarClientesCommand
+    {
+        public int CD_Cliente { get; set; }
+
+        public string Nome_RazaoSocial { get; set; }
+
+        public int ID_Tipo { get; set; }
+
+        public string Tipo { get; set; }
+
+        public string CPF_CNPJ { get; set; }
+
+        public string Conta { get; set; }
+
+        public string Email { get; set; }
+
+        public int ID_Status { get; set; }
+
+        public string Status { get; set; }
+    }
+}
--- API_Limites/Controllers/Outputs/Consultas.cs
+++ API_Limites/Controllers/Outputs/Consultas.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Hosting;
 using System.Collections;
 using API_Limites.Comandos;
+using API_Limites.Controllers.Inputs;
 
 namespace API_Limites.Controllers.Outputs
 {
@@ -25,7 +26,7 @@ namespace API_Limites.Controllers.Outputs
             _env = env;
         }
 
-        //Listar todos os clientes
+        //Listar todos os clientes e saldos
         [HttpGet]
         [Route("Clientes e Saldos")]
         public JsonResult ListarClientesESaldos()
@@ -76,41 +77,94 @@ namespace API_Limites.Controllers.Outputs
 
             return new JsonResult(resultado);
         }
+        //Fim - Listar todos os clientes e saldos
+
+        //Listar todos os clientes
+        [HttpGet]
+        [Route("Clientes")]
+        public JsonResult ListarClientes()
+        {
+            var resultado = new List<ListarClientesCommand>();
+            SqlDataReader reader;
+            string query = @"SELECT [CD_Cliente],[Nome_RazaoSocial],[ID_Tipo],[Tipo],[CPF_CNPJ],[Conta],[Email],[ID_Status],[Status] FROM [dbo].[VW_LIMITES]";
+
+            using (SqlConnection conexao = new SqlConnection(_configuration.GetConnectionString("APICon")))
+            {
+                conexao.Open();
+
+                using (SqlCommand lista = new SqlCommand(query, conexao))
+                {
+                    reader = lista.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            resultado.Add(new ListarClientesCommand
+                            {
+                                CD_Cliente = reader.GetInt32(0),
+                                Nome_RazaoSocial = reader.GetString(1),
+                                ID_Tipo = reader.GetInt32(2),
+                                Tipo = reader.GetString(3),
+                                CPF_CNPJ = reader.GetString(4),
+                                Conta = reader.GetString(5),
+                                Email = reader.GetString(6),
+                                ID_Status = reader.GetInt32(7),
+                                Status = reader.GetString(8),
+                                
+                            });
+
+                        }
+                    }
+
+                    else
+                    {
+                        return new JsonResult("Não há clientes cadastrados.");
+                    }
+                    reader.Close();
+                    conexao.Close();
+                }
+            }
+
+            return new JsonResult(resultado);
+        }
         //Fim - Listar todos os clientes
 
 
         //Buscar um cliente por ID
-        //[HttpGet("{CD_Cliente}")]
-        //public JsonResult GetID(int CD_Cliente)
+        //[HttpGet("{id}")]
+        //[Route("Buscar Cliente")]
+        //public JsonResult BuscaCliente (int id)
         //{
-        //    var resultado = new List<Clientes_Consultas>();
+        //    var resultado = new List<BuscaClienteCommand>();
         //    SqlDataReader reader;
+        //    string query = @"SELECT [Nome_RazaoSocial],[ID_Tipo],[Tipo],[CPF_CNPJ],[Conta],[Email],[ID_Status],[Status],[Limite_Total],[Limite_Tomado],[Limite_Disponivel] from [dbo].[VW_LIMITES] WHERE [CD_Cliente] = @CD_Cliente";
 
         //    using (SqlConnection conexao = new SqlConnection(_configuration.GetConnectionString("APICon")))
         //    {
         //        conexao.Open();
 
-        //        using (SqlCommand getIDcommand = new SqlCommand(@"SELECT [CD_Cliente], [Nome_RazaoSocial], [ID_Tipo], [CPF_CNPJ], [Conta], [Email], [Status], [Limite_Total], [Limite_Tomado], [Limite_Disponivel] from [dbo].[VW_LIMITES] WHERE [CD_Cliente] = @CD_Cliente", conexao))
+        //        using (SqlCommand getIDcommand = new SqlCommand(query, conexao))
         //        {
-        //            getIDcommand.Parameters.AddWithValue("@CD_Cliente", CD_Cliente);
+        //            getIDcommand.Parameters.AddWithValue("@CD_Cliente", id);
         //            reader = getIDcommand.ExecuteReader();
         //            if (reader.HasRows)
         //            {
         //                while (reader.Read())
         //                {
-        //                    resultado.Add(new Clientes_Consultas
+        //                    resultado.Add(new BuscaClienteCommand
         //                    {
-
-        //                        CD_Cliente = reader.GetInt32(0),
-        //                        Nome_RazaoSocial = reader.GetString(1),
-        //                        ID_Tipo = reader.GetString(2),
+        //                        //CD_Cliente = reader.GetInt32(0),
+        //                        Nome_RazaoSocial = reader.GetString(0),
+        //                        ID_Tipo = reader.GetInt32(1),
+        //                        Tipo = reader.GetString(2),
         //                        CPF_CNPJ = reader.GetString(3),
         //                        Conta = reader.GetString(4),
         //                        Email = reader.GetString(5),
-        //                        Status = reader.GetString(6),
-        //                        Limite_Total = reader.GetDecimal(7),
-        //                        Limite_Tomado = reader.GetDecimal(8),
-        //                        Limite_Disponivel = reader.GetDecimal(9)
+        //                        ID_Status = reader.GetInt32(6),
+        //                        Status = reader.GetString(7),
+        //                        Limite_Total = reader.GetDecimal(8),
+        //                        Limite_Tomado = reader.GetDecimal(9),
+        //                        Limite_Disponivel = reader.GetDecimal(10)
         //                    });
         //                }
         //            }
